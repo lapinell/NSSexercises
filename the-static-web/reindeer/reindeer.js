1f9e771911398649276cd49c28210a360cb2a6e0
lapinell <lapinell@gmail.com>
2018-01-18 11:07:44 -0600

    completed exercise - woot!

---
+++ the-static-web/reindeer/reindeer.js
@@ -0,0 +1,14 @@
+var colors = ["Blue", "Red", "Orange", "Purple", "Hazel", "Aquamarine", "Periwinkle", "Azure", "Fuchsia", "Chocolate", "Amber", "Amaranth"];
+var reindeer = ["Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"];
+
+for (i=0; i < reindeer.length; i++) {
+
+    var colorfulReindeer =  colors[i] + " " + reindeer[i] + "<br>";
+
+    var hohohoElement = document.getElementById("reindeer");
+
+    hohohoElement.innerHTML += colorfulReindeer;
+
+}
+
+console.log("All is working");
