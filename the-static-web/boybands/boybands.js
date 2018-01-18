8cd422b4cd7ee10e89c83e4d968df843181bae31
lapinell <lapinell@gmail.com>
2018-01-18 10:27:26 -0600

    finished exercises

--- the-static-web/boybands/boybands.js
+++ the-static-web/boybands/boybands.js
@@ -2,21 +2,26 @@ let bands = ["Boyz II Men", "NSync", "New Kids on the Block", "98 Degrees", "One
 let vegetables = ["Carrots", "Kale", "Zucchini", "Broccoli", "Squash"];
 
 // Get a reference to the appropriate DOM element for bands
-const bandElement = document.getElementById(???);
+const bandElement = document.getElementById("boy-bands");
 
 // Get a reference to the appropriate DOM element for vegetables
-const veggieElement = ???;
+const veggieElement = document.getElementById("vegetables");
+
+console.log(bands[0]);
 
 // Execute a for loop that will iterate over the arrays
-for (let loopTracker = 0; loopTracker < ???; loopTracker += 1) {
+for (let loopTracker = 0; loopTracker < 5; loopTracker += 1) {
 
   // Get a reference to the current item in the bands array
-  const currentBand = ???;
+  const currentBand = bands[loopTracker];
 
   // Update the innerHTML value of the DOM element for bands
+  bandElement.innerHTML += currentBand;
 
   // Get a reference to the current item in the vegetables array
-  const currentVeggie = ???;
+  const currentVeggie = vegetables[loopTracker];
 
   // Update the innerHTML value of the DOM element for vegetables
+  veggieElement.innerHTML += currentVeggie;
+  
 }
