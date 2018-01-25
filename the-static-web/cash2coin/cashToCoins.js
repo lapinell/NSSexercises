43e32400bbb5d439311066efdf3d544a42dcd136
lapinell <lapinell@gmail.com>
2018-01-25 10:06:16 -0600

    completed exercise

---
+++ the-static-web/cash2coin/cashToCoins.js
@@ -0,0 +1,21 @@
+const dollarAmount = 0.10
+const piggyBank = {}
+
+// Your magic code here
+
+piggyBank.quarters = 0 * 4;
+piggyBank.dimes = dollarAmount * 10;
+piggyBank.nickels = 0 * 20;
+piggyBank.pennies = 0 * 100;
+
+// End my code
+
+console.log(piggyBank)
+
+// Result should be similar to:
+// {
+//     quarters: 0,
+//     dimes: 1,
+//     nickels: 0,
+//     pennies: 0
+//   }
